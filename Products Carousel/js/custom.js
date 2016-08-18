2e12397878bb488ba5d28664dcfeedf187fd5a73
Jian Huang <jhuangsoftware@gmail.com>
2016-08-18 16:47:46 -0400

    First commit: Products Carousel to demo "AEM Mobile specific Cordova-enabled plug-ins", "AEM Mobile Gesture API".  Drop this article into any collect, the article will show sibling article thumbnails in a carousel.

---
+++ Products Carousel/js/custom.js
@@ -0,0 +1,64 @@
+function ProductsCarousel() {
+
+}
+
+ProductsCarousel.prototype.drawProductsCarousel = function (collection, ignoreEntity) {
+    var ThisClass = this;
+
+    // make sure collection is valid
+    if (collection.metadata.entityName) {
+        collection.getChildren(function (data) {
+            var collectionEntities = data;
+            var filteredCollectionEntities = {'entities': []};
+            var processedConnectionEntities = {'entities': []};
+
+            // filter out entity to be ignored
+            $.each(collectionEntities.entities, function () {
+                if (this.id != ignoreEntity.id) {
+                    filteredCollectionEntities.entities.push(this);
+                }
+            });
+
+            $.each(filteredCollectionEntities.entities, function () {
+                var thisEntity = this;
+
+                // turn ASYNC process to get entity thumbnail image into SYNC
+                ThisClass.getProductCarouselItemImage(thisEntity, function (data) {
+                    thisEntity.thumbnailimage = data;
+                    processedConnectionEntities.entities.push(thisEntity);
+
+                    if (filteredCollectionEntities.entities.length == processedConnectionEntities.entities.length) {
+                        var theTemplateScript = $('#template-products-carousel').html();
+                        var theTemplate = Handlebars.compile(theTemplateScript);
+                        $('.home').append(theTemplate(processedConnectionEntities));
+
+                        adobeDPS.Gesture.disableNavigation($('#products-carousel')[0]);
+                    }
+                });
+            });
+        });
+    }
+};
+
+ProductsCarousel.prototype.getProductCarouselItemImage = function (entity, callbackFunc) {
+    entity.getThumbnailImage(
+        100,
+        100,
+        function (data) {
+            callbackFunc(data);
+        },
+        function () {
+            // on error
+            callbackFunc();
+        }
+    );
+};
+
+document.addEventListener('deviceready', function () {
+    var productsCarouselObj = new ProductsCarousel();
+
+    var currentEntity = cq.mobile.context.entity;
+    var currentCollection = cq.mobile.context.collection;
+
+    productsCarouselObj.drawProductsCarousel(currentCollection, currentEntity);
+}, false);
