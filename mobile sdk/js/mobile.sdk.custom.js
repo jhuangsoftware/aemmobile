a6089b9e8f4c320f057a55b3842a6de810af1786
Jian Huang <jhuangsoftware@gmail.com>
2016-11-03 11:11:21 -0400

    UPDATED: custom analytics code

--- mobile sdk/js/mobile.sdk.custom.js
+++ mobile sdk/js/mobile.sdk.custom.js
@@ -1,30 +1,18 @@
-document.addEventListener("deviceready", function() {
+document.addEventListener('deviceready', function() {
     // Mobile SDK is now initialized, write your code in here
-    ADB.trackAction("someTrackAction hit #1",
-                    [
-                        {
-                            "my.test1": "some test data #1"
-                        },
-                        {
-                            "my.testEvent1": "1"
-                        }
-                    ]);
-    ADB.trackState("someTrackState hit #2",
-                   [
-                        {
-                            "my.test1": "some test data #2"
-                        },
-                        {
-                            "my.testEvent1": "1"
-                        }  
-                    ]);
-    ADB.getVersion(
-                    function (value) {
-                        var versionNum = value;
-                        console.log(versionNum);
-                    }, 
-                    function () {
-                        var versionNum = 1.0;
-                        console.log(versionNum);
-                    });
+    ADB.trackAction('someTrackAction hit #1', {'my.test1': 'some test data #1'});
+    
+    ADB.trackAction('someTrackAction hit #2', {'my.test1': 'some test data #1', 'my.testEvent1': '1'});
+    
+    ADB.trackState('someTrackState hit #1', {'my.test1': 'some test data #2'});
+    
+    ADB.trackState('someTrackState hit #2', {'my.test1': 'some test data #2', 'my.testEvent1': '1'});
+    
+    ADB.getVersion(function (value) {
+        var versionNum = value;
+        console.log(versionNum);
+    }, function () {
+        var versionNum = 1.0;
+        console.log(versionNum);
+    });
 }, false);
